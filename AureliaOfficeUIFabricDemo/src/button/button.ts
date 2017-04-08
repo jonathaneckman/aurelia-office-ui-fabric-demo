d2aa4ec68ebe622cea2095e9447959a548648ded
Jonathan Eckman <jonathan.eckman.dev@gmail.com>
2017-04-08 13:05:40 -0400

    Imported react. Stubbed aurelia component wrapper for office ui react component.

---
+++ AureliaOfficeUIFabricDemo/src/button/button.ts
@@ -0,0 +1,52 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { customElement, autoinject, bindable, noView } from 'aurelia-framework';
+import { Button } from 'office-ui-fabric-react';
+
+
+//{
+//    "name": "office-ui-fabric-react",
+//        "path": "../node_modules/office-ui-fabric-react/lib-amd",
+//            "main": "index"
+//}
+
+
+@noView()
+@autoinject()
+//@bindable('data')
+@customElement('au-ms-button')
+export class ReactElement {
+
+    reactComponent = {};
+    element: Element;
+
+    constructor(element: Element) {
+        this.element = element;
+    }
+
+    render() {
+        this.reactComponent = ReactDOM.render(
+            React.createElement(Button, {}, null),
+            this.element
+        );
+    }
+
+    bind() {
+        this.render();
+    }
+
+    /**
+     * Data Changed
+     * 
+     * An automatic callback function when our "data"
+     * bindable value changes. We need to rebind the React
+     * element to get the new data from the ViewModel.
+     * 
+     * @param {any} newVal The updated data
+     * @returns {void}
+     * 
+     */
+    dataChanged(newVal) {
+        this.bind();
+    }
+}
